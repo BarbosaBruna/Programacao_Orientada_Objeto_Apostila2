42ef465b585cd0785957ad44fefa6b097f951812
Bruna Aparecida <Bruna@MACECOC02V57EVH.local>
2022-11-14 22:45:12 -0300

    POO(Apostila02) Ex2

--- Jogo.java
+++ Jogo.java
@@ -3,16 +3,22 @@ public class Jogo {
     public static void main(String[] args) {
         Personagem cacador = new Personagem("Cacador", 10, 0, 0);
         Personagem soneca = new Personagem("Soneca", 2, 6, 4); 
+        Personagem hunter = new Personagem("Jose", 9, 2, 1);
 
         while(true){
             cacador.cacar();
             cacador.comer();
             cacador.dormir();
+            System.out.println("\n====================");
             soneca.cacar();
             soneca.comer();
             soneca.dormir();
             System.out.println("\n====================");
-            // Thread.sleep(3000);
+            hunter.cacar();
+            hunter.comer();
+            hunter.dormir();
+            System.out.println("\n====================");
+           // Thread.sleep(3000);
         }   
     }
     
